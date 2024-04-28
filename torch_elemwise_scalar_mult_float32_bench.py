7d6ec56e034b4f170baaa9299a5b30b9e02cded0
Jinwon Pyo <gch99517@gmail.com>
2024-04-28 18:14:19 +0900

    Add more benchmarks

---
+++ torch_elemwise_scalar_mult_float32_bench.py
@@ -0,0 +1,74 @@
+import torch
+import singleton_timer as st
+import csv
+
+def torch_elemwise_mult_bench():
+  timer = st.SingletonTimer()
+
+  batch_size = 10
+  dims = [2**i for i in range(0, 13)]
+
+  for i in range(5):
+    for dim in dims:
+      print(f"Iteration {i}, Dim={dim}")
+      x = torch.randn((batch_size, dim, dim), dtype=torch.float32)
+
+      x_gpu = x.clone().detach()
+
+      t = timer.start(tag=f'CPU Computation (M=K=N={dim})', category=f'CPU Computation (M=K=N={dim})', exclude=(i == 0))
+      z = torch.mul(x, 99)
+      timer.end(t)
+
+      assert (z[0][0][0] == x[0][0][0] * 99)
+
+      t = timer.start(tag=f'GPU HtoD (M=K=N={dim})', category=f'GPU HtoD (M=K=N={dim})', exclude=(i == 0))
+      x_gpu = x_gpu.to(torch.device("cuda:0"))
+      timer.end(t)
+
+      t = timer.start(tag=f'GPU Computation (M=K=N={dim})', category=f'GPU Computation (M=K=N={dim})', exclude=(i == 0))
+      z_gpu = torch.mul(x_gpu, 99)
+      timer.end(t)
+
+      assert (z_gpu[0][0][0] == x_gpu[0][0][0] * 99)
+
+      t = timer.start(tag=f'GPU DtoH (M=K=N={dim})', category=f'GPU DtoH (M=K=N={dim})', exclude=(i == 0))
+      z_gpu = z_gpu.to(torch.device("cpu"))
+      timer.end(t)
+      
+      assert torch.allclose(z, z_gpu, atol=1e-2)
+  
+  raw_data = timer.display_summary()
+    
+  data = []
+
+  data_cpu = ['CPU comp. ']
+  for dim in dims:
+    key = f'CPU Computation (M=K=N={dim})'
+    data_cpu.append(raw_data[key])
+  data.append(data_cpu)
+
+  data_gpu = ['GPU comp. ']
+  for dim in dims:
+    key = f'GPU Computation (M=K=N={dim})'
+    data_gpu.append(raw_data[key])
+  data.append(data_gpu)
+
+  data_gpu_htod = ['GPU htod. ']
+  for dim in dims:
+    key = f'GPU HtoD (M=K=N={dim})'
+    data_gpu_htod.append(raw_data[key])
+  data.append(data_gpu_htod)
+
+  data_gpu_dtoh = ['GPU dtoh. ']
+  for dim in dims:
+    key = f'GPU DtoH (M=K=N={dim})'
+    data_gpu_dtoh.append(raw_data[key])
+  data.append(data_gpu_dtoh)
+
+  f = open('torch_elemwise_scalar_mult_cpu_vs_gpu_float32.csv', 'w')
+  writer = csv.writer(f)
+  writer.writerows(data)
+  f.close()
+
+if __name__=='__main__':
+  torch_elemwise_mult_bench()
